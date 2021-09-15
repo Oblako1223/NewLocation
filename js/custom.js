b894573adf6038e54f6c3da9c586805d2f6be568
Maksymilian Rękawek <43018609+Oblako1223@users.noreply.github.com>
2021-09-15 21:40:23 +0200

    next

--- js/custom.js
+++ js/custom.js
@@ -72,32 +72,12 @@ $(function() {
         });
     };
 
-    // Sub Menu
-    $('.bauen-main-menu li.bauen-sub>a').on('click', function () {
-        $(this).removeAttr('href');
-        var element = $(this).parent('li');
-        if (element.hasClass('open')) {
-            element.removeClass('open');
-            element.find('li').removeClass('open');
-            element.find('ul').slideUp();
-        }
-        else {
-            element.addClass('open');
-            element.children('ul').slideDown();
-            element.siblings('li').children('ul').slideUp();
-            element.siblings('li').removeClass('open');
-            element.siblings('li').find('li').removeClass('open');
-            element.siblings('li').find('ul').slideUp();
-        }
-    });
-    $('.bauen-main-menu>ul>li.bauen-sub>a').append('<span class="holder"></span>');
 
     // Document on load.
     $(function () {
         burgerMenu();
         mobileMenuOutsideClick();
     });
-
 
 
     // Navbar scrolling background
